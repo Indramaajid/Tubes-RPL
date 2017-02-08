84132b156857a5e250a977967d91c01bc505d7f9
maajidindra@gmail.com <maajidindra@gmail.com>
2017-02-08 23:34:36 +0700

    versi 1.0.0

---
+++ app/src/main/java/com/example/ina/aplikasipengajuanperjalanandinas/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.ina.aplikasipengajuanperjalanandinas;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+    EditText ed1, ed2;
+    TextView tx1;
+    int counter = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.button);
+        ed1 = (EditText) findViewById(R.id.username);
+        ed2 = (EditText) findViewById(R.id.password);
+        tx1 = (TextView) findViewById(R.id.counter);
+        tx1.setVisibility(View.GONE);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ed1.getText().toString().equals("iin") &&
+                        ed2.getText().toString().equals("iin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Wrong Password", Toast.LENGTH_SHORT).show();
+                    tx1.setVisibility(View.VISIBLE);
+                    tx1.setBackgroundColor(Color.RED);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        b1.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+
+    }
+}
